5686df12997df502964e7777e66f85572b672aa9
PxllPie <adwinugroho1@gmail.com>
2019-07-10 21:19:25 +0700

    add register

--- src/screen/auth/register.js
+++ src/screen/auth/register.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
-import { Text, View, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, View, TextInput, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Image, Button } from 'react-native'
+import AntDesign from 'react-native-vector-icons/dist/AntDesign'
 import HeaderBack from '../../components/headerBack'
 class Register extends Component {
     render() {
         return (
             <React.Fragment>
-                <HeaderBack title="kembali" navigation={this.props.navigation} />
-                <View style={{ flex: 1, paddingHorizontal: 21, paddingTop: 20 }}>
-                    <TextInput style={styles.input} placeholder='password now' />
-                    <TextInput style={styles.input} placeholder='new password' />
-                    <TextInput style={styles.input} placeholder='repeat the new password' />
-                    <TouchableOpacity style={{ flex: 1, alignItems: 'center', paddingTop: 30 }}>
-                        <Text style={{ fontSize: 14, }}>foget password ..?</Text>
+                <HeaderBack title="Buat Profil Baru" navigation={this.props.navigation} />
+                <View style={{marginTop: 20,}}>
+                    <TouchableOpacity style={{width: '50%', alignSelf: 'center'}}>
+                        <AntDesign style={styles.addPhoto} name="adduser" size={40} color={'#4287f5'} />
+                        <Text style={styles.addText}>Tambah Foto Profil</Text>
                     </TouchableOpacity>
+                </View>
+                <View style={{ paddingHorizontal: 21, paddingTop: 20 }}>
+
+                    <TextInput style={styles.input} placeholder='Nama Pengguna' />
+                    <TextInput style={styles.input} placeholder='Kata Sandi' />
+                    <TextInput style={styles.input} placeholder='Kota' />
+                    <TextInput style={styles.input} placeholder='Email' />
+
+                </View>
+                <View style={{ paddingHorizontal: 21, paddingTop: 30 }}>
+                    <Button 
+                        title="Buat Akun Baru"
+                    />
                 </View>
             </React.Fragment>
         )
@@ -22,9 +34,24 @@ export default Register
 const styles = StyleSheet.create({
     input: {
         borderBottomWidth: 1,
-        borderBottomColor: '#f71616',
+        borderBottomColor: '#4287f5',
         color: '#747678',
         paddingTop: 8,
         fontSize: 14,
+        marginTop: 10
+    },
+    addPhoto: {
+        alignSelf: 'center',
+        borderWidth: 2,
+        borderColor: '#4287f5',
+        borderRadius: 150,
+        padding: 25,
+        borderStyle: "dashed"
+    },
+    addText: {
+        color: '#4287f5',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        marginTop: 10,
     }
 })
