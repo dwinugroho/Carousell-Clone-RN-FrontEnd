5da93b03c5075998d5abc9bc194b42a79184160c
PxllPie <adwinugroho1@gmail.com>
2019-07-10 19:43:58 +0700

    fixing wishlist

--- src/screen/wishlist.js
+++ src/screen/wishlist.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Text, View, FlatList, TouchableOpacity, Image, StyleSheet, } from 'react-native'
+import { Text, View, FlatList, TouchableOpacity, Image, StyleSheet,TouchableWithoutFeedback } from 'react-native'
 import HeaderBack from '../components/headerBack'
 import Icon from 'react-native-vector-icons/dist/AntDesign'
+import Entypo from 'react-native-vector-icons/dist/Entypo'
 const data = [
     {
         'key': '1',
@@ -64,41 +65,70 @@ const data = [
 class FlatListItem extends Component {
     render() {
         return (
-            <View style={{flex :1}}>
-                <View style={{flex :1,backgroundColor: '#FFFFFF',paddingHorizontal:17,
-            margin: 14,borderRadius: 5,elevation :10}}>
-                <Image
-                    style={{height :120,width :120}}
-                    resizeMode="cover"
-                    source={{ uri: this.props.item.image }}
+            <View style={{flex :1, margin: 10, borderRadius: 7, borderWidth: 2, borderColor: '#f5f5f5'}}>
+                <View
+                    style={{width: '100%', height: 200, borderRadius: 8}}
+                >
+                    <Image
+                        style={{height: 200, borderRadius: 8}}
+                        resizeMode="cover"
+                        source={{ uri: this.props.item.image }}
                     />
-                    <Text>{this.props.item.name}</Text>
-                    <Text>{this.props.item.harga}</Text>
-                    <Text>{this.props.item.deskripsi}</Text>
-                    <View style={{borderBottomColor: 'black',borderBottomWidth: 1,paddingTop:3}}></View>
-                    <View style={{flexDirection: 'row',justifyContent: 'space-between',padding:15,backgroundColor:'center'}}>
-                        <Icon name='hearto' size={25} color={'#f72d2d'}/>
-                        <Icon name='totop' size={25} color={'#c2c4be'}/>
+
                 </View>
+                <Text style={{marginHorizontal: 5, marginTop: 5, color: 'black'}}>Sandal</Text>
+                <Text style={{marginHorizontal: 5, color: 'black'}}>Rp. 100.890</Text>
+                <View style={{flexDirection: 'row', margin: 5}}>
+                    <Image
+                        style={styles.profilePicture}
+                        source={{uri: 'https://cdn.dribbble.com/users/1044993/screenshots/5848337/penguin_dribbble.png'}} 
+                    />
+                    <View>
+                        <Text style={{fontWeight: 'bold', color: 'grey', fontSize: 12}}>PixellPie</Text>
+                        <Text style={{color: 'grey', fontSize: 10}}>3 menit lalu</Text>
                     </View>
-          </View>
+                </View>
+                <View style={{flexDirection: 'row', borderTopWidth: 1, borderColor: 'rgba(0,0,0,0.2)', paddingVertical: 10}}>
+                    <View style={{flex: 1, alignItems: 'center', borderRightWidth: 1, borderColor: 'rgba(0,0,0,0.2)'}}>
+                        <TouchableOpacity onPress={() => {console.warn('berhasil')}}>
+                            <Icon name='heart' size={15} color={'red'} /> 
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{flex: 1, alignItems: 'center'}}>
+                        <TouchableOpacity onPress={() => {console.warn('berhasil')}}>
+                            <Entypo name='dots-three-vertical' size={15} color={'grey'} /> 
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+                
+            </View>
            
         )
     }
 }
+
+const styles = StyleSheet.create({
+    profilePicture: {
+        width: 30,
+        height: 30,
+        borderRadius: 50,
+        marginRight: 7,
+    },
+})
 
 
 export default class Card extends Component {
     render() {
         return (
             <View>
-              <HeaderBack title="setting" navigation={this.props.navigation}/>
+              <HeaderBack title="Wishlist" navigation={this.props.navigation}/>
                 <FlatList
                     data={data}
                     numColumns={2}
                     renderItem={({ item, index }) => {
                         return (
-                            <FlatListItem navigation={this.props.navigation} navigation={this.props.navigation} item={item} index={index} />
+                            <FlatListItem navigation={this.props.navigation} item={item} index={index} />
                         )
                     }}
                 />
@@ -106,20 +136,4 @@ export default class Card extends Component {
         )
     }
 }
-const styles = StyleSheet.create({
-    parentCard: {
-        flex: 1,
-        borderWidth: 1,
-        borderColor: '#F5F5F5',
-    },
-    productImage: {
-        width: '100%',
-        height: '100%',
-    },
-    imageWrap: {
-        width: '80%',
-        height: 200,
-        margin: 20,
-    }
-})
 
