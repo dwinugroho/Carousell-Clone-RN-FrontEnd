fac06d11becb6b2e4d60cb773bb8ffb877027189
PxllPie <adwinugroho1@gmail.com>
2019-07-12 21:05:48 +0700

    cart

--- src/public/action/cart.js
+++ src/public/action/cart.js
@@ -7,3 +7,10 @@ export const getCart = (id_user) => {
         payload: axios.get(`${IP}/cart/${id_user}`)
     }
 }
+
+export const deleteCart = (data) => {
+	return {
+		type: 'DELETE_CART',
+		payload: axios.delete(`${IP}/cart?id_user=${data.id_user}&id_product=${data.id_product}`)
+	}
+}
